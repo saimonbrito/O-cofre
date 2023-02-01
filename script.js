0e982e0bbe2de58c298d1016cbea59d53b40ddda
saimon kintochi <britomauricio89@gmail.com>
2023-02-01 12:54:18 -0300

    primeiro comitt

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+let senhaSecreta = 20;
+// let senha = prompt('Digite um a senha ')
+    
+//     if(senhaSecreta == senha){
+
+//         alert(senhaSecreta +' Parabens você acertou !..')
+    
+//     }else if(senha > senhaSecreta){18
+    
+    
+//         alert('Errou.. senha invalida, tente um numero menor ')
+    
+//     }else if(senha < senhaSecreta){
+    
+//         alert('Errou.. senha invalida, tente um numero maior ')
+//     }
+
+// while(senha != senhaSecreta){
+    
+//     var senha = prompt('Digite um a senha ')
+
+//     if(senhaSecreta == senha){
+
+//         alert(senhaSecreta +' Parabens você acertou !..')
+    
+//     }else if(senha > senhaSecreta){
+    
+    
+//         alert('Errou.. senha invalida, tente um numero menor ')
+    
+//     }else if(senha < senhaSecreta){
+    
+//         alert('Errou.. senha invalida, tente um numero maior ')
+//     }
+  
+// }
