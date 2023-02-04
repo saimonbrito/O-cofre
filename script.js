bf6fa62f29aa6d21b23a2f63dec5a035e4f94763
saimon kintochi <britomauricio89@gmail.com>
2023-02-03 23:36:34 -0300

    friday

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+var mudar = document.querySelector('#mmm').value
+
+alert('teste de h1' + mudar )
+
+//console.log(mudar)
+//  const click=()=>{
+//     alert('cofre destravado')
+// }
+// //incirir elgo no html : document.write('so teste ')
+// mudar.addEventListener("click", (evt)=>{
+//     const el = evt.target
+//     el.classList.add('h1')
+// })
 
 
-let senhaSecreta = 20;
+
+//let senhaSecreta = 20;
 // let senha = prompt('Digite um a senha ')
     
 //     if(senhaSecreta == senha){
@@ -16,6 +30,8 @@ let senhaSecreta = 20;
     
 //         alert('Errou.. senha invalida, tente um numero maior ')
 //     }
+
+
 
 // while(senha != senhaSecreta){
     
@@ -36,3 +52,30 @@ let senhaSecreta = 20;
 //     }
   
 // }
+
+
+
+/////////////////////////////////////
+
+// var jogar = prompt('Quer jogar?(s/n)');
+// if(jogar == 's'){
+  
+//   var numeroSecreto = parseInt(Math.random() * (1001 - 1) + 1);
+//   var iterador = 0;
+//   console.log(numeroSecreto);
+  
+//   while(chute != numeroSecreto){
+    
+//     if(iterador != 0){
+//       alert('Você já fez' + iterador + 'tentativas!')
+//     }
+    
+//     var chute = prompt('Digite um número entre 1 e 1000');
+    
+//     if (chute == numeroSecreto){
+//       alert('Acertou!');
+//     } else if (chute > numeroSecreto){
+//          alert('O número secreto é menor que ' + chute);
+//     } else if (chute < numeroSecreto){
+//          alert('O número secreto é maior que ' + chute);
+//     }
